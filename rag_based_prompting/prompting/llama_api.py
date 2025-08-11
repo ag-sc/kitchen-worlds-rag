35127f2cae201622207f1220bdf11e40ac6bf3f5
Janfiderheld <Janfitoe@web.de>
2025-08-11 16:02:07 +0200

    add planning api for Llama similar to the existing vlm_tool

--- rag_based_prompting/prompting/llama_api.py
+++ rag_based_prompting/prompting/llama_api.py
@@ -1,16 +1,22 @@
+from typing import List
+
 import torch
 import transformers
 
 from RAG4Robots.src.manager import RAGManager
-from rag_based_prompting.prompting.prompter import Prompter
+from pybullet_planning.vlm_tools.vlm_api import VLMApi
 
 
-class LlamaClusterPrompter(Prompter):
-    def __init__(self, rag: RAGManager, max_new_tokens=400):
-        super().__init__("Llama-3.3-70B-Instruct", rag)
-        self.tokenizer = transformers.AutoTokenizer.from_pretrained(f'meta-llama/{self.get_model_name()}')
+class LlamaClusterApi(VLMApi):
+    name = "Llama 3.3 (Cluster)"
+
+    def __init__(self, rag: RAGManager, **kwargs):
+        super(LlamaClusterApi, self).__init__(**kwargs)
+        self.model_name = "Llama-3.3-70B-Instruct"
+        self.rag_manager = rag
+
+        self.tokenizer = transformers.AutoTokenizer.from_pretrained(f'meta-llama/{self.model_name}')
         self.tokenizer.pad_token_id = self.tokenizer.eos_token_id
-        self.max_new_tokens = max_new_tokens
         bnb_config = transformers.BitsAndBytesConfig(
             load_in_4bit=True,
             bnb_4bit_quant_type="nf4",
@@ -18,7 +24,7 @@ class LlamaClusterPrompter(Prompter):
             bnb_4bit_use_double_quant=True,
         )
         model_id = transformers.AutoModelForCausalLM.from_pretrained(
-            f'meta-llama/{self.get_model_name()}',
+            f'meta-llama/{self.model_name}',
             quantization_config=bnb_config,
             device_map="auto",
             trust_remote_code=True,
@@ -31,11 +37,11 @@ class LlamaClusterPrompter(Prompter):
             device_map="auto",  # finds GPU
         )
 
-    def prompt_model(self, system_msg: str, user_msg: str) -> str:
-        # Instruct version needs specific conversation structure
+    def _ask(self, prompt: str, image_path: str = "None", continue_chat: bool = True, max_tokens: int = 1000,
+             temperature: float = 0.0, **kwargs):
         messages = [
-            {"role": "system", "content": system_msg},
-            {"role": "user", "content": user_msg},
+            {"role": "system", "content": kwargs.get("sys_msg", "")},
+            {"role": "user", "content": prompt},
         ]
         prompt = self.generation_pipe.tokenizer.apply_chat_template(
             messages,
@@ -48,24 +54,28 @@ class LlamaClusterPrompter(Prompter):
         ]
         outputs = self.generation_pipe(
             prompt,
-            max_new_tokens=self.max_new_tokens,
+            max_new_tokens=max_tokens,
             eos_token_id=terminators,
             do_sample=False,
-            temperature=self.get_temperature(),
+            temperature=temperature,
             top_p=None
         )
         result = outputs[0]["generated_text"][len(prompt):]
         return result
 
 
-class LlamaLocalPrompter(Prompter):
-    def __init__(self, rag: RAGManager, max_new_tokens=400):
-        super().__init__("Llama-3.2-1B-Instruct", rag)
-        self.tokenizer = transformers.AutoTokenizer.from_pretrained(f'meta-llama/{self.get_model_name()}')
+class LlamaLocalApi(VLMApi):
+    name = "Llama 3.2 (Local)"
+
+    def __init__(self, rag: RAGManager, **kwargs):
+        super(LlamaLocalApi, self).__init__(**kwargs)
+        self.model_name = "Llama-3.2-1B-Instruct"
+        self.rag_manager = rag
+
+        self.tokenizer = transformers.AutoTokenizer.from_pretrained(f'meta-llama/{self.model_name}')
         self.tokenizer.pad_token_id = self.tokenizer.eos_token_id
-        self.max_new_tokens = max_new_tokens
         model_id = transformers.AutoModelForCausalLM.from_pretrained(
-            f'meta-llama/{self._model_name}',
+            f'meta-llama/{self.model_name}',
             torch_dtype=torch.float32,  # CPU-friendly
             device_map={"": "cpu"},  # Force CPU usage
             trust_remote_code=True
@@ -78,11 +88,11 @@ class LlamaLocalPrompter(Prompter):
             device_map={"": "cpu"},  # Explicit CPU mapping
         )
 
-    def prompt_model(self, system_msg: str, user_msg: str) -> str:
-        # Instruct version needs specific conversation structure
+    def _ask(self, prompt: str, image_path: str = "None", continue_chat: bool = True, max_tokens: int = 2500,
+             temperature: float = 0.0, **kwargs):
         messages = [
-            {"role": "system", "content": system_msg},
-            {"role": "user", "content": user_msg},
+            {"role": "system", "content": kwargs.get("sys_msg", "")},
+            {"role": "user", "content": prompt},
         ]
         prompt = self.generation_pipe.tokenizer.apply_chat_template(
             messages,
@@ -95,11 +105,19 @@ class LlamaLocalPrompter(Prompter):
         ]
         outputs = self.generation_pipe(
             prompt,
-            max_new_tokens=self.max_new_tokens,
+            max_new_tokens=max_tokens,
             eos_token_id=terminators,
             do_sample=False,
-            temperature=self.get_temperature(),
+            temperature=temperature,
             top_p=None
         )
         result = outputs[0]["generated_text"][len(prompt):]
         return result
+
+
+def get_context_through_rag(rag: RAGManager, query: str) -> List[str]:
+    cont_rank = rag.query_all_dbs(query)
+    contexts = []
+    for cont in cont_rank:
+        contexts.append(cont[0])
+    return contexts
--- rag_based_prompting/tests/test_prompting.py
+++ rag_based_prompting/tests/test_prompting.py
@@ -1,21 +1,24 @@
 from RAG4Robots.src.manager import RAGManager
 from RAG4Robots.src.utils.enums import ResourceType
-from rag_based_prompting.prompting.llama_prompter import LlamaLocalPrompter
-from rag_based_prompting.scenario import CurrentScenario
+from pybullet_planning.vlm_tools.prompts_gpt4v import list_of_actions_with_preconditions
+from rag_based_prompting.prompting.llama_api import LlamaLocalApi
+from rag_based_prompting.prompting.prompts import *
 
 
 def test_action_sequence_gen(task="make chicken soup"):
     rags = [(ResourceType.RECIPES, 0.5)]
     rag = RAGManager(rags)
-    prompter = LlamaLocalPrompter(rag)
+    prompter = LlamaLocalApi(rag)
 
-    scen = CurrentScenario(task, 2, ['bottle', 'pan', 'pot', 'lid', 'chicken broth', 'spoon'],
-                           ['bottle', 'pan', 'pot', 'lid', 'chicken broth', 'spoon'])
-    res = prompter.prompt_model_for_scenario(scen)
+    objects = ['bottle', 'pan', 'pot', 'lid', 'chicken broth', 'spoon']
+    prompt = build_user_msg_nl(contexts=prompter.rag_manager.query_all_dbs(task)).format(objects=str(objects),
+                                                                                         observed=str(objects))
+    sys_msg = system_prompt_nl.format(goal=task, n_arms=2)
+    res = prompter._ask(prompt, prompt_name='test_action_sequence_gen', **dict(sys_msg=sys_msg, image_name=None))
     print(res)
 
 
-def test_sequence_translation():
+def test_sequence_translation(task="make chicken soup"):
     # Example output from 'test_action_sequence_gen("make chicken soup")'
     actions = """1. Open the pot. 
     2. Pick up the pot.
@@ -44,12 +47,14 @@ def test_sequence_translation():
     25. Put the spoon in the pot."""
     rags = [(ResourceType.RECIPES, 0.5)]
     rag = RAGManager(rags)
-    prompter = LlamaLocalPrompter(rag)
+    prompter = LlamaLocalApi(rag)
 
-    scen = CurrentScenario("make chicken soup", 2, ['bottle', 'pan', 'pot', 'lid', 'chicken broth', 'spoon'],
-                           ['bottle', 'pan', 'pot', 'lid', 'chicken broth', 'spoon'])
-    scen.set_actions(actions)
-    res = prompter.prompt_model_for_scenario(scen)
+    objects = ['bottle', 'pan', 'pot', 'lid', 'chicken broth', 'spoon']
+    prompt = build_user_msg_fl(contexts=prompter.rag_manager.query_all_dbs(task)).format(actions=actions,
+                                                                                         set_of_actions=list_of_actions_with_preconditions,
+                                                                                         objects=str(objects))
+    res = prompter._ask(prompt, prompt_name='test_action_sequence_gen',
+                        **dict(sys_msg=system_prompt_fl, image_name=None))
     print(res)
 
 
