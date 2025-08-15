090b57d3460892cfe4c11f1a21c45c0f0843989a
Janfiderheld <Janfitoe@web.de>
2025-08-15 16:21:06 +0200

    prepare folders for cluster experiments

--- pybullet_planning/vlm_tools/problems_vlm_tamp.py
+++ pybullet_planning/vlm_tools/problems_vlm_tamp.py
@@ -45,6 +45,8 @@ def get_problem_dict_from_open_goal(world, objects, args, load_llm_memory=None, 
     rag_kwargs['rag_wikihow'] = args.rag_wikihow
     rag_kwargs['rag_cutting_vids'] = args.rag_cutting_vids
     rag_kwargs['rag_cskg_locations'] = args.rag_cskg_locations
+    if len(args.exp_name) == 0:
+        args.exp_name = f'seed_{args.seed}'
     llamp_api = llamp_api_class(open_goal, planning_mode=planning_mode, load_memory=load_llm_memory, seed=args.seed,
                                 log_dir=log_dir, vlm_kwargs=dict(image_dir=img_dir), rag_kwargs=rag_kwargs)
     problem_dict = {'english_goal': open_goal, 'llamp_api': llamp_api}
