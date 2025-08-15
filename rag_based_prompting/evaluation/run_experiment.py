b6720de2880b1cc46ff2f06c9846ae99b888073c
Janfiderheld <Janfitoe@web.de>
2025-08-15 15:36:11 +0200

    use seeds from file for experiments

--- rag_based_prompting/evaluation/run_experiment.py
+++ rag_based_prompting/evaluation/run_experiment.py
@@ -4,6 +4,8 @@ from pybullet_planning.tutorials.test_vlm_tamp import get_vlm_tamp_agent_parser_
 from pybullet_planning.vlm_tools import run_vlm_tamp_with_argparse
 
 
+SEED_PATH = '../eval_scenarios/seeds.txt'
+
 def update_parser(conf):
     parser = get_vlm_tamp_agent_parser_given_config(conf)
     parser.add_argument("--rag_recipes", type=float, choices=[Range(0.0, 1.0)], default=0.5,
@@ -12,20 +14,24 @@ def update_parser(conf):
                         help="What percentage of the WikiHow database should be used in RAG?")
     parser.add_argument("--rag_cutting_vids", type=float, choices=[Range(0.0, 1.0)], default=0.5,
                         help="What percentage of the cutting tutorial videos should be used in RAG?")
-    parser.add_argument("--rag_cskg_locations", type=float, choices=[Range(0.0, 1.0)], default=0.5,
+    parser.add_argument("--rag_cskg_locations", type=float, choices=[Range(  0.0, 1.0)], default=0.5,
                         help="What percentage of the CSKG Locations should be used in RAG?")
     return parser
 
 
 def run_experiments():
-    run_vlm_tamp_with_argparse(get_agent_parser_given_config=update_parser)
+    with open(SEED_PATH, "r") as f:
+        seeds = [int(line.strip()) for line in f]
+
+    for s in seeds:
+        run_vlm_tamp_with_argparse(get_agent_parser_given_config=update_parser, seed=s)
 
 
 def generate_seeds_for_experiment(n=100):
     seeds = []
     for i in range(n):
         seeds.append(random.randint(0, 10 ** 6 - 1))
-    with open("../eval_scenarios/seeds.txt", "w") as f:
+    with open(SEED_PATH, "w") as f:
         f.writelines(f"{seed}\n" for seed in seeds)
     print(f'Finished generating {n} seeds')
 
