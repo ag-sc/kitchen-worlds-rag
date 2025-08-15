76a6ba5cf126cccd6a603d9760abe960d71f5bc1
Janfiderheld <Janfitoe@web.de>
2025-08-15 16:29:33 +0200

    make path to seed file more robust

--- rag_based_prompting/evaluation/run_experiment.py
+++ rag_based_prompting/evaluation/run_experiment.py
@@ -1,4 +1,5 @@
 import random
+from os import path
 
 from pybullet_planning.tutorials.test_vlm_tamp import get_vlm_tamp_agent_parser_given_config
 from pybullet_planning.vlm_tools import run_vlm_tamp_with_argparse
